93d30348e12dd00337e55ddb50832415f1f158ea
HIMANSHU <67531739+pythoner-code@users.noreply.github.com>
2023-11-06 09:42:34 +0530

    Create Exp8.m

---
+++ Exp8.m
@@ -0,0 +1,16 @@
+vidReader = VideoReader('visiontraffic.avi','CurrentTime',11);
+opticFlow = opticalFlowLK('NoiseThreshold',0.009);
+h = figure;
+movegui(h);
+hViewPanel = uipanel(h,'Position',[0 0 1 1],'Title','Plot of Optical Flow Vectors');
+hPlot = axes(hViewPanel);
+while hasFrame(vidReader)
+    frameRGB = readFrame(vidReader);
+    frameGray = im2gray(frameRGB);
+    flow = estimateFlow(opticFlow,frameGray);
+    imshow(frameRGB)
+    hold on
+    plot(flow,'DecimationFactor',[5 5],'ScaleFactor',10,'Parent',hPlot);
+    hold off
+    pause(10^-3)
+end
