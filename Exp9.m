f0b0db77dae1411a5f3c089f58231ff335c9ca72
HIMANSHU <67531739+pythoner-code@users.noreply.github.com>
2023-11-06 09:54:49 +0530

    Create Exp9.m

---
+++ Exp9.m
@@ -0,0 +1,167 @@
+unzip vehicleDatasetImages.zip
+data = load("vehicleDatasetGroundTruth.mat");
+vehicleDataset = data.vehicleDataset;
+vehicleDataset(1:4,:)
+vehicleDataset.imageFilename = fullfile(pwd,vehicleDataset.imageFilename);
+rng("default");
+shuffledIndices = randperm(height(vehicleDataset));
+idx = floor(0.6 * length(shuffledIndices) );
+
+trainingIdx = 1:idx;
+trainingDataTbl = vehicleDataset(shuffledIndices(trainingIdx),:);
+
+validationIdx = idx+1 : idx + 1 + floor(0.1 * length(shuffledIndices) );
+validationDataTbl = vehicleDataset(shuffledIndices(validationIdx),:);
+
+testIdx = validationIdx(end)+1 : length(shuffledIndices);
+testDataTbl = vehicleDataset(shuffledIndices(testIdx),:);
+imdsTrain = imageDatastore(trainingDataTbl{:,"imageFilename"});
+bldsTrain = boxLabelDatastore(trainingDataTbl(:,"vehicle"));
+
+imdsValidation = imageDatastore(validationDataTbl{:,"imageFilename"});
+bldsValidation = boxLabelDatastore(validationDataTbl(:,"vehicle"));
+
+imdsTest = imageDatastore(testDataTbl{:,"imageFilename"});
+bldsTest = boxLabelDatastore(testDataTbl(:,"vehicle"));
+trainingData = combine(imdsTrain,bldsTrain);
+validationData = combine(imdsValidation,bldsValidation);
+testData = combine(imdsTest,bldsTest);
+
+validateInputData(trainingData);
+validateInputData(validationData);
+validateInputData(testData);
+
+data = read(trainingData);
+I = data{1};
+bbox = data{2};
+annotatedImage = insertShape(I,"Rectangle",bbox);
+annotatedImage = imresize(annotatedImage,2);
+figure
+imshow(annotatedImage)
+reset(trainingData);
+inputSize = [608 608 3];
+className = "vehicle";
+rng("default")
+trainingDataForEstimation = transform(trainingData,@(data)preprocessData(data,inputSize));
+numAnchors = 9;
+[anchors,meanIoU] = estimateAnchorBoxes(trainingDataForEstimation,numAnchors);
+area = anchors(:, 1).*anchors(:,2);
+[~,idx] = sort(area,"descend");
+
+anchors = anchors(idx,:);
+anchorBoxes = {anchors(1:3,:)
+    anchors(4:6,:)
+    anchors(7:9,:)
+    };
+
+
+detector = yolov4ObjectDetector("csp-darknet53-coco",className,anchorBoxes,InputSize=inputSize);
+augmentedTrainingData = transform(trainingData,@augmentData);
+augmentedData = cell(4,1);
+for k = 1:4
+    data = read(augmentedTrainingData);
+    augmentedData{k} = insertShape(data{1},"rectangle",data{2});
+    reset(augmentedTrainingData);
+end
+figure
+montage(augmentedData,BorderSize=10)
+options = trainingOptions("adam",...
+    GradientDecayFactor=0.9,...
+    SquaredGradientDecayFactor=0.999,...
+    InitialLearnRate=0.001,...
+    LearnRateSchedule="none",...
+    MiniBatchSize=4,...
+    L2Regularization=0.0005,...
+    MaxEpochs=70,...
+    BatchNormalizationStatistics="moving",...
+    DispatchInBackground=true,...
+    ResetInputNormalization=false,...
+    Shuffle="every-epoch",...
+    VerboseFrequency=20,...
+    ValidationFrequency=1000,...
+    CheckpointPath=tempdir,...
+    ValidationData=validationData);
+
+doTraining = false;
+if doTraining       
+    % Train the YOLO v4 detector.
+    [detector,info] = trainYOLOv4ObjectDetector(augmentedTrainingData,detector,options);
+else
+    % Load pretrained detector for the example.
+    detector = downloadPretrainedYOLOv4Detector();
+end
+I = imread("highway.png");
+[bboxes,scores,labels] = detect(detector,I);
+I = insertObjectAnnotation(I,"rectangle",bboxes,scores);
+figure
+imshow(I)
+metrics = evaluateObjectDetection(detectionResults,testData);
+classID = 1;
+precision = metrics.ClassMetrics.Precision{classID};
+recall = metrics.ClassMetrics.Recall{classID};
+figure
+plot(recall,precision)
+xlabel("Recall")
+ylabel("Precision")
+grid on
+title(sprintf("Average Precision = %.2f",metrics.ClassMetrics.mAP(classID)))
+
+
+function data = augmentData(A)
+data = cell(size(A));
+for ii = 1:size(A,1)
+    I = A{ii,1};
+    bboxes = A{ii,2};
+    labels = A{ii,3};
+    sz = size(I);
+
+    if numel(sz) == 3 && sz(3) == 3
+        I = jitterColorHSV(I,...
+            contrast=0.0,...
+            Hue=0.1,...
+            Saturation=0.2,...
+            Brightness=0.2);
+    end
+    
+    tform = randomAffine2d(XReflection=true,Scale=[1 1.1]);
+    rout = affineOutputView(sz,tform,BoundsStyle="centerOutput");
+    I = imwarp(I,tform,OutputView=rout);
+    
+    [bboxes,indices] = bboxwarp(bboxes,tform,rout,OverlapThreshold=0.25);
+    labels = labels(indices);
+    
+    if isempty(indices)
+        data(ii,:) = A(ii,:);
+    else
+        data(ii,:) = {I,bboxes,labels};
+    end
+end
+end
+
+function data = preprocessData(data,targetSize)
+for ii = 1:size(data,1)
+    I = data{ii,1};
+    imgSize = size(I);
+    
+    bboxes = data{ii,2};
+
+    I = im2single(imresize(I,targetSize(1:2)));
+    scale = targetSize(1:2)./imgSize(1:2);
+    bboxes = bboxresize(bboxes,scale);
+    
+    data(ii,1:2) = {I,bboxes};
+end
+end
+function detector = downloadPretrainedYOLOv4Detector()
+if ~exist("yolov4CSPDarknet53VehicleExample_22a.mat", "file")
+    if ~exist("yolov4CSPDarknet53VehicleExample_22a.zip", "file")
+        disp("Downloading pretrained detector...");
+        pretrainedURL = "https://ssd.mathworks.com/supportfiles/vision/data/yolov4CSPDarknet53VehicleExample_22a.zip";
+        websave("yolov4CSPDarknet53VehicleExample_22a.zip", pretrainedURL);
+    end
+    unzip("yolov4CSPDarknet53VehicleExample_22a.zip");
+end
+pretrained = load("yolov4CSPDarknet53VehicleExample_22a.mat");
+detector = pretrained.detector;
+end
+
